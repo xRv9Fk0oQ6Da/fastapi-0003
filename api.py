aeb31a76ff3eb0daceb83bf81dbc1f44f65deb03
Cryptic Cypher <GodsCorner@pm.me>
2022-06-09 02:56:32 -0400

    first commit

--- api.py
+++ api.py
@@ -1,26 +1,6 @@
-from fastapi import FastAPI, Path
-from typing import Optional
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
-
-students = {
-    1: {
-        "name": "John Doe",
-        "age": 18,
-        "year": "2020"
-    }
-}
-
-class Students(BaseModel):
-    name: str,
-    age: int,
-    year: str
-
-class Updatestudent(BaseModel):
-    name: Options[str] = None
-    age: Optional[int] = None
-    year: Optional[str] = None
 
 @app.get("/")
 def index():
